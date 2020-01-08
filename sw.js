e082a1a81be149a325ae771fea968566db71ee0b
githumbi <githumbi74@gmail.com>
2020-01-08 12:56:10 +0300

    edit

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-  '/portfolio/',
-  '/portfolio/index.html',
-  '/portfolio/js/*',
-  '/portfolio/css/*',
-  '/portfolio/images/*',
+  '/',
+  '/index.html',
+  '/js/*',
+  '/css/*',
+  '/images/*',
 ];
 // install event
 self.addEventListener('install', evt => {
